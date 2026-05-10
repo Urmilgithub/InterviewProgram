b23957ac8ee5e653f5c68e0a23442ad13a05b814
Urmilgithub <urmilpatelit27@gmail.com>
2026-05-10 23:36:51 +0530

    Palindrome String

---
+++ PalindromeString/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string str = "madam";
+        bool isPalindrome = true;
+
+        int left = 0;
+        int right = str.Length - 1;
+
+        while (left < right)
+        {
+            if (str[left] != str[right])
+            {
+                isPalindrome = false;
+                break;
+            }
+
+            left++;
+            right--;
+        }
+
+        if (isPalindrome)
+            Console.WriteLine("Palindrome");
+        else
+            Console.WriteLine("Not Palindrome");
+    }
+}
