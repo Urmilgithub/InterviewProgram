5bd6ee6cb96d7adb534e82fada51b4330d25e094
Urmilgithub <urmilpatelit27@gmail.com>
2026-05-10 23:34:01 +0530

    Add project files.

---
+++ ReverseString/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string str = "hello";
+        char[] arr = new char[str.Length];
+
+        int j = 0;
+
+        for (int i = str.Length - 1; i >= 0; i--)
+        {
+            arr[j] = str[i];
+            j++;
+        }
+
+        string reversed = new string(arr);
+
+        Console.WriteLine(reversed);
+    }
+}
