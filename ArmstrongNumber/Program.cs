7ea0148407b4bcb2aad51e04d46c622cddf3fea6
Urmilgithub <urmilpatelit27@gmail.com>
2026-05-11 22:44:11 +0530

    Armstrong Number

---
+++ ArmstrongNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num = 153;
+        int temp = num;
+        int sum = 0;
+
+        while (num > 0)
+        {
+            int rem = num % 10;
+            sum = sum + (rem * rem * rem);
+            num = num / 10;
+        }
+
+        if (temp == sum)
+            Console.WriteLine("Armstrong Number");
+        else
+            Console.WriteLine("Not Armstrong");
+    }
+}
