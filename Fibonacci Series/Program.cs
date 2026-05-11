190bce93e3e9825c543df5d96d6fe294f4f6ec49
Urmilgithub <urmilpatelit27@gmail.com>
2026-05-11 22:41:14 +0530

    Fibonacci Series

---
+++ Fibonacci Series/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 0, b = 1, c;
+
+        Console.Write(a + " " + b + " ");
+
+        for (int i = 1; i <= 8; i++)
+        {
+            c = a + b;
+            Console.Write(c + " ");
+
+            a = b;
+            b = c;
+        }
+    }
+}
