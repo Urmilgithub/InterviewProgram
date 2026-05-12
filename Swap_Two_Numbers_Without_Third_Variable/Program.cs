d17b5ce5b55545687942f32df6bdfe54db9fd5d3
Urmilgithub <urmilpatelit27@gmail.com>
2026-05-12 23:47:13 +0530

    Swap Two Numbers Without Third Variable

---
+++ Swap_Two_Numbers_Without_Third_Variable/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 10;
+        int b = 20;
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        Console.WriteLine("a = " + a);
+        Console.WriteLine("b = " + b);
+    }
+}
