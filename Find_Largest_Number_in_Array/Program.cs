269580e076307e4e9c5d40b82c1ad8e43dd6ea80
Urmilgithub <urmilpatelit27@gmail.com>
2026-05-13 22:46:20 +0530

    Largest Element in Array

---
+++ Find_Largest_Number_in_Array/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 12, 45, 7, 89, 23 };
+
+        int max = arr[0];
+
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] > max)
+            {
+                max = arr[i];
+            }
+        }
+
+        Console.WriteLine("Largest Number: " + max);
+    }
+}
