71bba024e74f8b39d8a9ed85b05e895d548abeba
Urmilgithub <urmilpatelit27@gmail.com>
2026-05-13 23:07:07 +0530

    Second Largest Element in Array

---
+++ Second_Largest_Number_in_Array/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 10, 50, 30, 80, 60 };
+
+        int largest = arr[0];
+        int secondLargest = arr[0];
+
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] > largest)
+            {
+                secondLargest = largest;
+                largest = arr[i];
+            }
+            else if (arr[i] > secondLargest && arr[i] != largest)
+            {
+                secondLargest = arr[i];
+            }
+        }
+
+        Console.WriteLine("Second Largest: " + secondLargest);
+    }
+}
