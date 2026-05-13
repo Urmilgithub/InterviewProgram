2aa643abb76b6458a61670119ab08f0c3738da86
Urmilgithub <urmilpatelit27@gmail.com>
2026-05-13 22:57:07 +0530

    Vowels in String

---
+++ Count_Vowels_in_String/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string str = "education";
+        int count = 0;
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            char ch = str[i];
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' ||
+                ch == 'o' || ch == 'u' ||
+                ch == 'A' || ch == 'E' || ch == 'I' ||
+                ch == 'O' || ch == 'U')
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine("Vowel Count: " + count);
+    }
+}
