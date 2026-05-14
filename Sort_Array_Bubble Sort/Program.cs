e4a5d64e6410602f86caf16fd8850573d913cb6f
Urmilgithub <urmilpatelit27@gmail.com>
2026-05-14 23:36:28 +0530

    Sort Array Bubble Sort

---
+++ Sort_Array_Bubble Sort/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 5, 2, 8, 1, 9 };
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            for (int j = 0; j < arr.Length - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        Console.WriteLine("Sorted Array:");
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write(arr[i] + " ");
+        }
+    }
+}
