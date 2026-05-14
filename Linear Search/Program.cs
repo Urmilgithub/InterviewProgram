458184cf4e855c17c411a39bfd1c048553d2ad5c
Urmilgithub <urmilpatelit27@gmail.com>
2026-05-14 23:46:20 +0530

    Linear Search

---
+++ Linear Search/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 10, 20, 30, 40 };
+        int search = 30;
+        int found = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] == search)
+            {
+                found = 1;
+                break;
+            }
+        }
+
+        if (found == 1)
+            Console.WriteLine("Element Found");
+        else
+            Console.WriteLine("Element Not Found");
+    }
+}
