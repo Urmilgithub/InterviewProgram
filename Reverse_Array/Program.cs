f5ea084d5196ba86bf2ad5dd2c50abb39405c9cc
Urmilgithub <urmilpatelit27@gmail.com>
2026-05-15 23:23:52 +0530

    Reverse Array

---
+++ Reverse_Array/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 1, 2, 3, 4, 5 };
+
+        for (int i = arr.Length - 1; i >= 0; i--)
+        {
+            Console.Write(arr[i] + " ");
+        }
+    }
+}
