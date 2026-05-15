b4808c0319665bc0ef7b546441a4947150fd1f77
Urmilgithub <urmilpatelit27@gmail.com>
2026-05-15 23:18:39 +0530

    Sum of Array Element

---
+++ Sum_of_Array_Elements/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        int sum = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            sum += arr[i];
+        }
+
+        Console.WriteLine("Sum: " + sum);
+    }
+}
