943865bada37e30c8d797a53601cf9a958608614
Urmilgithub <urmilpatelit27@gmail.com>
2026-05-16 23:15:51 +0530

    Square Star Pattern

---
+++ Square_Star_Pattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 4;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write("* ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
