594fceedde7087a686a9a52a548e4bf7dc768128
Urmilgithub <urmilpatelit27@gmail.com>
2026-05-16 23:18:34 +0530

    Pyramid Pattern

---
+++ Pyramid_Pattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 4;
+
+        for (int i = 1; i <= n; i++)
+        {
+            // spaces
+            for (int j = 1; j <= n - i; j++)
+            {
+                Console.Write(" ");
+            }
+
+            // stars
+            for (int k = 1; k <= (2 * i - 1); k++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
