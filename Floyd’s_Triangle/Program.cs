99e68d6d37b1a8f12632a1f1473b35185b019ed8
Urmilgithub <urmilpatelit27@gmail.com>
2026-05-17 23:32:03 +0530

    Floyd’s Triangle

---
+++ Floyd’s_Triangle/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int rows = 5;
+        int number = 1;
+
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(number + " ");
+                number++;
+            }
+            Console.WriteLine();
+        }
+    }
+}
