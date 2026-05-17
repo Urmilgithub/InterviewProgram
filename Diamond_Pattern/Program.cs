ef371fd4f23cd01dda39b2736dacc90ed897d100
Urmilgithub <urmilpatelit27@gmail.com>
2026-05-17 23:38:42 +0530

    Diamond Pattern

---
+++ Diamond_Pattern/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int rows = 5;
+
+        // Upper part
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = i; j < rows; j++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int k = 1; k <= (2 * i - 1); k++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+
+        // Lower part
+        for (int i = rows - 1; i >= 1; i--)
+        {
+            for (int j = rows; j > i; j--)
+            {
+                Console.Write(" ");
+            }
+
+            for (int k = 1; k <= (2 * i - 1); k++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
