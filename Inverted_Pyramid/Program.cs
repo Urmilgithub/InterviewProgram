eb3613ce52ee307c10845991be05385bfd18f381
Urmilgithub <urmilpatelit27@gmail.com>
2026-05-18 23:47:51 +0530

    Inverted Pyramid

---
+++ Inverted_Pyramid/Program.cs
@@ -0,0 +1,16 @@
+﻿int n = 5;
+
+for (int i = n; i >= 1; i--)
+{
+    for (int j = 1; j <= n - i; j++)
+    {
+        Console.Write(" ");
+    }
+
+    for (int j = 1; j <= (2 * i - 1); j++)
+    {
+        Console.Write("*");
+    }
+
+    Console.WriteLine();
+}
