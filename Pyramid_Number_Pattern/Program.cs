9860ca8548d19075e13a33782cadf33c23f1cf3a
Urmilgithub <urmilpatelit27@gmail.com>
2026-05-19 23:39:08 +0530

    Pyramid Number Pattern

---
+++ Pyramid_Number_Pattern/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 4;
+
+        for (int i = 1; i <= n; i++)
+        {
+            // spaces
+            for (int j = 1; j <= n - i; j++)
+            {
+                Console.Write(" ");
+            }
+
+            // increasing numbers
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(j);
+            }
+
+            // decreasing numbers
+            for (int j = i - 1; j >= 1; j--)
+            {
+                Console.Write(j);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
