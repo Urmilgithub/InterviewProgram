8a445227b3f511e369a5b68fcce94f8b3c874a67
Urmilgithub <urmilpatelit27@gmail.com>
2026-05-20 23:38:28 +0530

    Centered Number Pyramid

---
+++ Centered_Number_Pyramid/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int rows = 4;
+
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int s = 1; s <= rows - i; s++)
+                Console.Write(" ");
+
+            for (int j = 1; j <= (2 * i - 1); j++)
+                Console.Write(i);
+
+            Console.WriteLine();
+        }
+    }
+}
