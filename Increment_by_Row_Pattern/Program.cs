340989af9d5de4435923485bbc41d93131ffc3f9
Urmilgithub <urmilpatelit27@gmail.com>
2026-05-20 23:36:28 +0530

    Increment by Row Pattern

---
+++ Increment_by_Row_Pattern/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int rows = 4;
+        int num = 1;
+
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(num);
+                num++;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
